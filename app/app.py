cc94389e0e7d0f8a9d399d88f48c38ad88e9ed23
Brayan Henao <b.henaoc@uniandes.edu.co>
2023-04-23 16:08:15 -0500

    Update metrics to allow Gunicorn server workers

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flasgger import Swagger
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 from config import config_app
 from models.db import db
@@ -26,7 +26,7 @@ def import_blueprints(flask_app):
 
 
 app = Flask(__name__)
-PrometheusMetrics(app)
+GunicornInternalPrometheusMetrics(app)
 CORS(app)
 import_blueprints(app)
 Swagger(app)
