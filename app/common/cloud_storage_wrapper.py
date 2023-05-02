87232454e95870d63767836205afaad3a98a1f8b
Felipe Cerquera <a.cerquera@uniandes.edu.co>
2023-05-02 17:42:24 -0500

    feat: added Cloud Storage API to store the files (#5)
    * feat: added Cloud Storage API to store the files
    
    * refactor: removed unused code

---
+++ app/common/cloud_storage_wrapper.py
@@ -0,0 +1,37 @@
+from google.cloud import storage
+import io
+
+
+class CloudStorageWrapper:
+    client = storage.Client()
+    bucket_name = 'misw4204-aplicaciones-en-la-nube'
+
+    def __init__(self):
+        self.bucket = self.client.get_bucket(self.bucket_name)
+
+    # Get file as bytes from bucket
+    def get_file(self, file_name):
+        blob = self.bucket.blob(file_name)
+        return blob.download_as_bytes()
+
+    # Download a file into local filename
+    def get_file_into_filename(self, file_name, output_filename):
+        blob = self.bucket.blob(file_name)
+        return blob.download_to_filename(output_filename)
+
+    # Upload a bytes-like file to bucket
+    def upload_file(self, file_name, file):
+        blob = self.bucket.blob(file_name)
+        file.seek(0)
+        bytes_file = io.BytesIO(file.read())
+        blob.upload_from_file(bytes_file, content_type=file.content_type)
+
+    # Upload a file from path to bucket
+    def upload_file_from_path(self, file_name, source_file_name):
+        blob = self.bucket.blob(file_name)
+        blob.upload_from_filename(filename=source_file_name)
+
+    # Delete file from bucket
+    def delete_file(self, file_name):
+        blob = self.bucket.blob(file_name)
+        blob.delete()
--- app/services/files_service.py
+++ app/services/files_service.py
@@ -1,5 +1,6 @@
 from common.exceptions import CustomException, ResourceNotFoundException
 from common.decorators import db_session
+from common.cloud_storage_wrapper import CloudStorageWrapper
 from flask import send_from_directory
 from models.task import Task
 from schemas.task_schema import TaskSchema
@@ -8,13 +9,14 @@ from schemas.task_schema import TaskSchema
 @db_session
 def get_file_by_id(session, id_task, selected='converted'):
     task = session.query(Task).filter_by(user_id=id_user, id=id_task).first()
+    cs_wrapper = CloudStorageWrapper()
     if task is None:
         raise ResourceNotFoundException('Task not found')
     if selected == 'original':
-        return send_from_directory('static/files/uploaded', str(task.id) + '.' + task.extension_from,
-                                   as_attachment=True)
+        file = cs_wrapper.get_file('files/uploaded/' + str(task.id) + '.' + task.extension_from)
+        return file
     elif selected == 'converted':
-        return send_from_directory('static/files/converted',
-                                   str(task.id) + '.' + task.extension_to, as_attachment=True)
+        file = cs_wrapper.get_file('files/converted/' + str(task.id) + '.' + task.extension_to)
+        return file
     else:
         raise ResourceNotFoundException('File not found')
